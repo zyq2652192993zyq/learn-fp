1698c01da0f30df61fb7c8b7c425fef97526dce8
yzhang68 <yzhang68@ebay.com>
2023-02-20 21:29:27 +0800

    finish Show

--- src/main/scala/learnfp/typeclass/Show.scala
+++ src/main/scala/learnfp/typeclass/Show.scala
@@ -1,25 +1,20 @@
 package learnfp.typeclass
 
 trait Show[A] {
-  def show(x:A):String
+  def show(x: A): String
 }
 
 object Printer {
-  def show[A](x:A)(implicit showInstance:Show[A]):String = {
+  def show[A](x: A)(implicit showInstance: Show[A]): String = {
     showInstance.show(x)
   }
 }
 
 object ShowInstances {
-  implicit val intInstance:Show[Int] = new Show[Int] {
-    override def show(x: Int): String = ???
-  }
+  implicit val intInstance: Show[Int] = (x: Int) => x.toString
 
-  implicit val doubleInstance:Show[Double] = new Show[Double] {
-    override def show(x: Double): String = ???
-  }
+  implicit val doubleInstance: Show[Double] = (x: Double) => x.toString
 
-  implicit def listInstance[T](implicit xShow:Show[T]):Show[List[T]] = new Show[List[T]] {
-    override def show(xs:List[T]): String = ???
-  }
+  implicit def listInstance[T](implicit xShow: Show[T]): Show[List[T]] = (xs: List[T]) =>
+    s"[${xs.map(xShow.show).mkString(", ")}]"
 }

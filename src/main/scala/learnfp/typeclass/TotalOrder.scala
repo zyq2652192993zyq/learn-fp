eebdb51dd636fedf56f94511fca392837177c2ba
yzhang68 <yzhang68@ebay.com>
2023-02-20 21:25:00 +0800

    finish TotalOrder

--- src/main/scala/learnfp/typeclass/TotalOrder.scala
+++ src/main/scala/learnfp/typeclass/TotalOrder.scala
@@ -1,26 +1,30 @@
 package learnfp.typeclass
 
 trait TotalOrder[A] {
-  def less(lhs:A, rhs:A):Boolean
+  def less(lhs: A, rhs: A): Boolean
 }
 
 object TotalOrderInstances {
-  implicit val intInstance:TotalOrder[Int] = new TotalOrder[Int] {
-    override def less(lhs: Int, rhs: Int): Boolean = ???
-  }
+  implicit val intInstance: TotalOrder[Int] = (lhs: Int, rhs: Int) => lhs < rhs
 
-  implicit val stringInstance:TotalOrder[String] = new TotalOrder[String] {
-    override def less(lhs: String, rhs: String): Boolean = ???
-  }
+  implicit val stringInstance: TotalOrder[String] = (lhs: String, rhs: String) => lhs.compareTo(rhs) < 0
 
-  implicit def listInstance[T](implicit suborder:TotalOrder[T]):TotalOrder[List[T]] = new TotalOrder[List[T]] {
-    override def less(lhs: List[T], rhs: List[T]): Boolean = ???
-  }
+  /**
+    * the official answer is not clear, what if lists have different length or one of them is empty
+    */
+  implicit def listInstance[T](implicit suborder: TotalOrder[T]): TotalOrder[List[T]] = (lhs: List[T], rhs: List[T]) =>
+//    lhs.zip(rhs).forall({ case (l, r) => suborder.less(l, r) })
+    (lhs, rhs) match {
+      case (Nil, Nil)               => false
+      case (lHead :: _, rHead :: _) => suborder.less(lHead, rHead)
+      case (Nil, rHead :: _)        => true
+      case (lHead :: _, Nil)        => false
+    }
 }
 
 object Comparator {
   @annotation.implicitNotFound("No instance of TotalOrder found")
-  def less[A](lhs:A, rhs:A)(implicit order:TotalOrder[A]) = {
+  def less[A](lhs: A, rhs: A)(implicit order: TotalOrder[A]) = {
     order.less(lhs, rhs)
   }
 }
--- src/main/scala/learnfp/typeclass/TypeClass.scala
+++ src/main/scala/learnfp/typeclass/TypeClass.scala
@@ -1,21 +1,17 @@
 package learnfp.typeclass
 
 trait TypeClass[A] {
-  def foo(x:A):String
+  def foo(x: A): String
 }
 
 object TypeClassInstances {
-  implicit val intInstance:TypeClass[Int] = new TypeClass[Int] {
-    override def foo(x:Int): String = "int"
-  }
+  implicit val intInstance: TypeClass[Int] = (x: Int) => "int"
 
-  implicit val stringInstance:TypeClass[String] = new TypeClass[String] {
-    override def foo(x:String): String = "string"
-  }
+  implicit val stringInstance: TypeClass[String] = (x: String) => "string"
 }
 
 object TypeClassUser {
-  def foo[A](x:A)(implicit typeClass:TypeClass[A]):String  = {
+  def foo[A](x: A)(implicit typeClass: TypeClass[A]): String = {
     typeClass.foo(x)
   }
 }

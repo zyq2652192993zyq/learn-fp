7929cc136db29d587204b36180705653eeda50e7
zyq2652192993zyq <2652192993@qq.com>
2023-04-05 16:01:01 +0800

    feat: accomplish SimpleMonid

--- src/main/scala/learnfp/monoid/SimpleMonoid.scala
+++ src/main/scala/learnfp/monoid/SimpleMonoid.scala
@@ -1,15 +1,25 @@
 package learnfp.monoid
 
 object SimpleMonoid {
-  case class Sum(value:Int)
-  implicit val sumMonoidInstance:Monoid[Sum] = new Monoid[Sum] {
-    override def mzero: Sum = ???
-    override def mappend(lhs: Sum, rhs: Sum): Sum = ???
+  case class Sum(value: Int)
+  implicit val sumMonoidInstance: Monoid[Sum] = new Monoid[Sum] {
+    override def mzero: Sum = Sum(0)
+
+    override def mappend(lhs: Sum, rhs: Sum): Sum = Sum(lhs.value + rhs.value)
   }
 
-  case class Product(value:Int)
-  implicit val productMonoidInstance:Monoid[Product] = new Monoid[Product] {
-    override def mzero: Product = ???
-    override def mappend(lhs: Product, rhs: Product): Product = ???
+  class SumMonoidOps(lhs: Sum)(implicit sumMonoid: Monoid[Sum]) {
+    def |+|(rhs: Sum): Sum = sumMonoid.mappend(lhs, rhs)
+  }
+
+  case class Product(value: Int)
+  implicit val productMonoidInstance: Monoid[Product] = new Monoid[Product] {
+    override def mzero: Product = Product(1)
+
+    override def mappend(lhs: Product, rhs: Product): Product = Product(lhs.value * rhs.value)
+  }
+
+  class ProductOps(lhs: Product)(implicit productMonoid: Monoid[Product]) {
+    def |+|(rhs: Product): Product = productMonoid.mappend(lhs, rhs)
   }
 }
